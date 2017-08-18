29445eae712b2dc2e5516b8c90408b04901c5c73
Andrej Lavrinovic <andrej.lavrinovic@SB-IE-T4K5HTDB.local>
2017-08-18 11:06:28 +0100

    main class is created

--- src/main/java/ie/altech/jersey/JerseyTutorialServer.java
+++ src/main/java/ie/altech/jersey/JerseyTutorialServer.java
@@ -1,8 +1,30 @@
 package ie.altech.jersey;
 
-public class JerseyTutorialServer {
+import com.hubspot.dropwizard.guice.GuiceBundle;
+import ie.altech.jersey.configuration.ServerConfiguration;
+import ie.altech.jersey.module.ServerModule;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
 
-    public static void main(String[] args){
+public class JerseyTutorialServer extends Application<ServerConfiguration> {
 
+    @Override
+    public void initialize(Bootstrap<ServerConfiguration> bootstrap){
+        GuiceBundle<ServerConfiguration> guiceBundle = GuiceBundle.<ServerConfiguration>newBuilder()
+                .addModule(new ServerModule())
+                .setConfigClass(ServerConfiguration.class)
+                .enableAutoConfig(getClass().getPackage().getName())
+                .build();
+        bootstrap.addBundle(guiceBundle);
+    }
+
+    @Override
+    public void run(ServerConfiguration serverConfiguration, Environment environment) throws Exception{
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        new JerseyTutorialServer().run(args);
     }
 }
